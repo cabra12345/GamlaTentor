4075e8833df7e1d56ba86f5ff6aaa1cfcd7021ab
cabra123245 <carljohanbratthall@gmail.com>
2025-01-07 19:02:19 +0100

    nu med manuella klasser

--- HönanEllerÄgget/HenHouse.cs
+++ HönanEllerÄgget/HenHouse.cs
@@ -14,7 +14,8 @@ namespace HönanEllerÄgget
     new Hen("Sjukhöns", 3, 632.5),
     new Hen("Svart Höns", 5, 550.0),
     new Hen("Vita Höns", 4, 600.0),
-    new Hen("Röd Höns", 6, 620.0)
+    new Hen("Röd Höns", 6, 620.0),
+    new Hen("Woop Woop", 69, 42.0),
         };
     }
 }
