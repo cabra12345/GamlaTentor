e9043d695410b057065c4ee89c04d0d169a5add0
cabra123245 <carljohanbratthall@gmail.com>
2025-01-07 18:29:28 +0100

    uppgift 4

---
+++ HönanEllerÄgget/Hen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HönanEllerÄgget
+{
+    public class Hen
+    {
+        public string Name { get; set; }
+        public int Stall { get; set; }
+        public double Weight { get; set; }
+        public Hen(string name, int stall, double weight)
+        {
+            Name = name;
+            Stall = stall;
+            Weight = weight;
+        }
+    }
+}
+
---
+++ HönanEllerÄgget/HenHouse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HönanEllerÄgget
+{
+    public class HenHouse
+    {
+        private List<Hen> _hens = new List<Hen>()
+        {
+    new Hen("Pärlhöns", 7, 675.5),
+    new Hen("Sjukhöns", 3, 632.5),
+    new Hen("Svart Höns", 5, 550.0),
+    new Hen("Vita Höns", 4, 600.0),
+    new Hen("Röd Höns", 6, 620.0)
+        };
+    }
+}
